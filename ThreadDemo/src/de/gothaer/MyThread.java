28da0cf3e7f4b11753e1549464c0f7ee47d0fcf9
limagoHub <limagoHub@limago.de>
2020-07-01 12:02:01 +0200

    initial commit

---
+++ ThreadDemo/src/de/gothaer/MyThread.java
@@ -0,0 +1,25 @@
+package de.gothaer;
+
+public class MyThread extends Object implements Runnable{
+	
+	private final String message;
+
+	public MyThread(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			for (int i = 0; i < 10; i++) {
+				System.out.println(message);
+				Thread.sleep((long)(Math.random()* 1000));
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+
+		}
+	}
+
+}
